ebf142a8c80b0cba4bd9fe124f802700daf8f316
habib9700 <digitalbusinesss.ci@gmail.com>
2026-05-20 13:36:54 +0000

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,3 @@
 export default function Home() {
-  return (
-    <main style={{padding:"30px",textAlign:"center"}}>
-      <h1>Digital Business CI</h1>
-
-      <p>
-        Gagnez de l'argent grâce aux produits digitaux
-      </p>
-
-      <a href="https://pay.wave.com/m/M_ci_YDLKef8UEhyp/c/ci/?amount=15000">
-        Payer avec Wave
-      </a>
-
-      <br /><br />
-
-      <a href="https://wa.me/2250799539408">
-        WhatsApp
-      </a>
-    </main>
-  );
+  return <h1>Digital Business CI fonctionne</h1>;
 }
